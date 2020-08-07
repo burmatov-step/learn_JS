ecd1cb9c87ca285d0cecb17b4429f4b8ab2dd0f9
burmatov-step <stepaburmatov@mail.ru>
2020-08-07 08:55:16 +0300

    lesson 2

--- script/main.js
+++ script/main.js
@@ -1,9 +1,17 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 100000;
+let income = '100000';
+let addExpenses = 'Интернет, Бензин, Коммуналка, Ипотека';
+let deposit = true;
+let mission = 10000000;
+let period = 12;
 
-alert ('Hi curator');
-console.log('Hello')
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев`, `Цель заработать ${mission} рублей`);
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = money/30;
+console.log(budgetDay)
