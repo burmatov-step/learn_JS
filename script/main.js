5c49f0e8492bd091e9cd09faf9edac51454cc636
burmatov-step <stepaburmatov@mail.ru>
2020-08-14 16:21:18 +0300

    remark

--- script/main.js
+++ script/main.js
@@ -88,4 +88,7 @@ if (appData.getTargetMonth() < 0) {
 
 console.log("Уровень дохода: ", appData.budget);
 
-
+console.log("Наша программа включает в себя данные:");
+for(let key in appData){
+  console.log(key + " : " + appData[key]);
+}
