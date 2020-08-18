58eeda471b02db6496d05e5d68951e146d2d48af
burmatov-step <stepaburmatov@mail.ru>
2020-08-18 10:45:00 +0300

    end lesson

---
+++ index.js
@@ -0,0 +1,39 @@
+const start = document.getElementById("start");
+
+const plusIncome = document.getElementsByTagName("button")[0];
+
+const plusExpenses = document.getElementsByTagName("button")[1];
+
+const depositCheck = document.querySelector("#deposit-check");
+
+const additionalIncome = document.querySelectorAll(".additional_income-item");
+
+const budgetMonth = document.getElementsByClassName("budget_month-value")[0];
+
+const budgetDay = document.getElementsByClassName("budget_day-value")[0];
+
+const expensesMonth = document.getElementsByClassName("expenses_month-value")[0];
+
+const additionalValue = document.getElementsByClassName("additional_income-value")[0];
+
+const additionalExpenses = document.getElementsByClassName("additional_expenses-value")[0];
+
+const incomePeriod = document.getElementsByClassName("income_period-value")[0];
+
+const targetMonth = document.getElementsByClassName("target_month-value")[0];
+
+const summBudget = document.querySelector(".salary-amount");
+
+const incomeTitle = document.querySelector(".income-title");
+
+const incomeAmount = document.querySelector(".income-amount");
+
+const expensesTitle = document.querySelector(".expenses-title");
+
+const expensesAmount = document.querySelector(".expenses-amount");
+
+const additionalExpensesItem = document.querySelector(".additional_expenses-item");
+
+const targetAmount = document.querySelector(".target-amount");
+
+const range = document.querySelector("[type='range']");
