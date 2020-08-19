da33fc15bdc1db076e9523720f3d564f136ba650
burmatov-step <stepaburmatov@mail.ru>
2020-08-19 10:42:44 +0300

    end leson

--- index.js
+++ index.js
@@ -1,169 +1,23 @@
-'use strict';
-
-const started = document.getElementById("start"),
-      plusIncome = document.getElementsByTagName("button")[0],
-      plusExpenses = document.getElementsByTagName("button")[1],
-      depositCheck = document.querySelector("#deposit-check"),
-      additionalIncome = document.querySelectorAll(".additional_income-item"),
-      budgetMonth = document.getElementsByClassName("budget_month-value")[0],
-      budgetDay = document.getElementsByClassName("budget_day-value")[0],
-      expensesMonth = document.getElementsByClassName("expenses_month-value")[0],
-      additionalValue = document.getElementsByClassName("additional_income-value")[0],
-      additionalExpenses = document.getElementsByClassName("additional_expenses-value")[0],
-      incomePeriod = document.getElementsByClassName("income_period-value")[0],
-      targetMonth = document.getElementsByClassName("target_month-value")[0],
-      summBudget = document.querySelector(".salary-amount"),
-      incomeTitle = document.querySelector(".income-title"),
-      incomeAmount = document.querySelector(".income-amount"),
-      expensesTitle = document.querySelector(".expenses-title"),
-      expensesAmount = document.querySelector(".expenses-amount"),
-      additionalExpensesItem = document.querySelector(".additional_expenses-item"),
-      targetAmount = document.querySelector(".target-amount"),
-      range = document.querySelector("[type='range']");
-
-let isNumber = function (n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-};
-let isSrting = function (n) {
-  return !isNaN(parseFloat(n)) || n === null
-    ? true
-    : Boolean(n.trim().length === 0);
-};
-let money,
-  start = function () {
-    do {
-      money = prompt("Ваш месячный доход?");
-    } while (!isNumber(money));
-  };
-start();
-
-let appData = {
-  budget: money,
-  budgetMonth: 0,
-  expensesMonth: 0,
-  income: {},
-  addIncome: [],
-  expenses: {},
-  addExpenses: [],
-  budgetDay: 0,
-  deposit: false,
-  percentDeposit: 0,
-  moneyDeposit: 0,
-  mission: 50000,
-  period: 3,
-  getExpensesMonth: function () {
-    let sum = 0;
-    for (let key in appData.expenses) {
-      sum += appData.expenses[key];
-    }
-
-    return sum;
-  },
-
-  getBudget: function () {
-    return (
-      (appData.budgetMonth = +appData.budget - appData.getExpensesMonth()),
-      (appData.budgetDay = Math.floor(appData.budgetMonth / 30))
-    );
-  },
-  getTargetMonth: function () {
-    return appData.mission / appData.budgetMonth;
-  },
-
-  getStatusIncome: function () {
-    if (appData.budgetDay > 1200) {
-      console.log("У вас высокий уровень дохода");
-    } else if (appData.budgetDay > 600 && appData.budgetDay <= 1200) {
-      console.log("У вас средний уровень дохода");
-    } else if (appData.budgetDay <= 600 && appData.budgetDay > 0) {
-      console.log("К сожалению у вас уровень дохода ниже среднего");
-    } else if (appData.budgetDay <= 0) {
-      console.log("Что то пошло не так");
-    }
-  },
-
-  asking: function () {
-    if (confirm("Есть ли у вас дополнительный заработок?")) {
-      let itemIncome;
-      do {
-        itemIncome = prompt(
-          "Какой у вас есть дополнительный заработок?",
-          "Такси"
-        );
-      } while (isSrting(itemIncome));
-
-      let cashIncome;
-      do {
-        cashIncome = prompt("Сколько в месяц зарабатываете на этом?", 10000);
-      } while (!isNumber(cashIncome));
-      appData.income[itemIncome] = cashIncome;
-    }
-
-    for (let i = 0; i < 2; i++) {
-      let num = 0;
-      let expenses = [];
-      do {
-        expenses[i] = prompt("ВВедите обязательную статью расходов?");
-      } while (isSrting(expenses[i]));
-
-      do {
-        num = prompt("Во сколько это обойдется?");
-        appData.expenses[expenses[i]] = +num;
-      } while (!isNumber(num));
-    }
-
-    let addExpenses = prompt(
-      "Перечислите возможные расходы за рассчитываемый период через запятую"
-    );
-    appData.addExpenses = addExpenses.toLowerCase().split(", ");
-    appData.deposit = confirm("Есть ли у вас депозит в банке?");
-  },
-  getInfoDeposit: function () {
-    if (appData.deposit) {
-      do {
-        appData.percentDeposit = prompt("Какой годовой процент?", "10");
-      } while (!isNumber(appData.percentDeposit));
-      do {
-        appData.moneyDeposit = prompt("Какая сумма заложена?", 10000);
-      } while (!isNumber(appData.moneyDeposit));
-    }
-  },
-  calcSavedMoney: function () {
-    return appData.budgetMonth * appData.period;
-  },
-};
-
-appData.asking();
-appData.getBudget();
-appData.getTargetMonth();
-console.log("Расходы за месяц: ", appData.getExpensesMonth());
-
-if (appData.getTargetMonth() < 0) {
-  console.log("Цель не будет достигнута");
-} else {
-  console.log(
-    "Период достижения цели в месяцах: ",
-    Math.ceil(appData.getTargetMonth())
-  );
-}
-
-console.log("Уровень дохода: ", appData.budget);
-
-console.log("Наша программа включает в себя данные:");
-for (let key in appData) {
-  console.log(key + " : " + appData[key]);
-}
-
-appData.getInfoDeposit();
-
-console.log(
-  appData.percentDeposit,
-  appData.moneyDeposit,
-  appData.calcSavedMoney()
-);
-
-for (let i = 0; i < appData.addExpenses.length; i++) {
-  appData.addExpenses[i] =
-    appData.addExpenses[i][0].toUpperCase() + appData.addExpenses[i].slice(1);
-}
-console.log(appData.addExpenses.join(", "));
+let collection = document.querySelectorAll(".book");
+let books = document.querySelector(".books");
+let heading = document.querySelectorAll("[target='_blank']");
+let promo = document.querySelector(".adv");
+let list2 = collection[0].querySelectorAll("li");
+let list5 = collection[5].querySelectorAll("li");
+let list6 =collection[2].querySelectorAll('li');
+let newElement = document.createElement('li');
+books.prepend(collection[1]);
+books.append(collection[2]);
+collection[4].after(collection[3])
+document.body.style.backgroundImage = "url(./image/you-dont-know-js.jpg)";
+heading[4].textContent = "Книга 3. this и Прототипы Объектов";
+promo.remove();
+list2[3].after(list2[6]);
+list2[4].before(list2[8]);
+list2[9].after(list2[2]);
+list5[1].after(list5[9]);
+list5[4].after(list5[2]);
+list5[7].after(list5[5]);
+newElement.textContent = "Глава 8: За пределами ES6";
+console.log(list6);
+list6[9].before(newElement);
