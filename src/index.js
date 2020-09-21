83fa4860edde1b79cf951241b4c8cae3840e8415
burmatov-step <stepaburmatov@mail.ru>
2020-09-21 12:40:21 +0300

    remark

--- src/index.js
+++ src/index.js
@@ -33,6 +33,10 @@ const rendCountry = () => {
   });
 
   function renderCity(arr) {
+    arr.sort((a, b) => {
+      return b.count - a.count;
+    });
+  //
     for (let i = 0; i < 3; i++) {
       const city = document.createElement("div");
       city.classList.add("dropdown-lists__line");
@@ -205,3 +209,24 @@ inputCities.addEventListener('click', (e)=>{
   }
 })
 
+var items = [
+  { name: "Edward", value: 21 },
+  { name: "Sharpe", value: 37 },
+  { name: "And", value: 45 },
+  { name: "The", value: -12 },
+  // { name: "Magnetic" },
+  { name: "Zeros", value: 37 },
+];
+items.sort(function (a, b) {
+ return b.value - a.value
+  // if (a.value > b.value) {
+  //   return 1;
+  // }
+  // if (a.value < b.value) {
+  //   return -1;
+  // }
+  // a должно быть равным b
+  // return 0;
+});
+
+console.log(items);
